96ce1c5972195652cecc6903eb89940bc15e3780
Khemraj Junior <khemrajuniordev@gmail.com>
2026-01-21 15:50:42 -0400

    Arros Functions

---
+++ Aula11-Arrows-Functions/arrow-functions.js
@@ -0,0 +1,16 @@
+
+// Function Expression antiga
+/*const calcularSoma = function(Valor1, Valor2) {
+    return Valor1 + Valor2;
+}
+
+//console.log(calcularSoma(5, 10)); // 15
+*/
+
+// Arrow Function atualizada
+const calcularSomaArrow = (Valor1, Valor2) => Valor1 + Valor2;
+const Valor1 = 10;
+const Valor2 = 20;
+
+const resuldado = calcularSomaArrow = (Valor1, Valor2)
+console.log(`O resuldado da soma de ${Valor1} + ${Valor2} é: ${resuldado}`)
---
+++ Aula11-Arrows-Functions/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+// Function Expression antiga
+const calcularSoma = function(Valor1, Valor2) {
+    return Valor1 + Valor2;
+}
+
+console.log(calcularSoma(5, 10)); // 15
